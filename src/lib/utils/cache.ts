b358dc268534989683494fbcb0eb68c8983320e3
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-05 06:32:06 +0300

    create isNumerator, isDenominator getter for mpt cache

--- src/lib/utils/cache.ts
+++ src/lib/utils/cache.ts
@@ -1,11 +1,19 @@
 import parser from "../parser";
 
+class UtilsCacheMPT {
+	public week: MPT.Week = "Числитель";
+
+	get isNumerator(): boolean {
+		return this.week === "Числитель";
+	}
+
+	get isDenominator(): boolean {
+		return this.week === "Знаменатель";
+	}
+}
+
 class UtilsCache {
-	public readonly mpt: {
-		week: MPT.Week;
-	} = {
-		week: "Числитель",
-	};
+	public readonly mpt = new UtilsCacheMPT();
 
 	public async update(): Promise<void> {
 		this.mpt.week = await parser.getCurrentWeek();
