9ce8cbcb8df99ec5c1ea25f904035e5e65fe4d8a
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-05 08:14:51 +0300

    add rest utils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,6 +2,7 @@ import UtilsMPT from "./mpt/index";
 import UtilsCache from "./cache";
 import UtilsEventEmitter from "./events";
 import UtilsVK from "./vk/index";
+import UtilsRest from "./rest";
 
 class Utils {
 	public readonly cache = new UtilsCache();
@@ -9,6 +10,7 @@ class Utils {
 	public readonly events = new UtilsEventEmitter();
 
 	public readonly vk = new UtilsVK();
+	public readonly rest = new UtilsRest();
 }
 
 export default new Utils();
---
+++ src/lib/utils/rest.ts
@@ -0,0 +1,57 @@
+import moment from "moment";
+
+class UtilsRest {
+	public readonly DayTemplates = [
+		/воскресенье|вс/,
+		/понедельник|пн/,
+		/вторник|вт/,
+		/среда|ср/,
+		/четверг|чт/,
+		/пятница|пт/,
+		/суббота|сб/,
+	] as const;
+
+	public parseSelectedDate(text = ""): moment.Moment {
+		let selectedDate: moment.Moment | undefined;
+		if (text === "" || /(?:^сегодня|с)$/gi.test(text)) {
+			selectedDate = moment();
+		} else if (/(?:^завтра|^з)$/gi.test(text)) {
+			selectedDate = moment().add(1, "day");
+		} else if (/(?:^послезавтра|^пз)$/gi.test(text)) {
+			selectedDate = moment().add(2, "day");
+		} else if (/(?:^вчера|^в)$/gi.test(text)) {
+			selectedDate = moment().subtract(1, "day");
+		} else if (/(?:^позавчера|^поз)$/gi.test(text)) {
+			selectedDate = moment().subtract(2, "day");
+		} else if (/([\d]+)?(.)?([\d]+)?(.)?([\d]+)/.test(text)) {
+			selectedDate = moment(text, "DD.MM.YYYY");
+		} else {
+			for (const templateIndex in this.DayTemplates) {
+				const Regular_Expression = new RegExp(
+					this.DayTemplates[templateIndex],
+					`gi`,
+				);
+				if (Regular_Expression.test(text) === true) {
+					const currentDate = new Date();
+					const targetDay = Number(templateIndex);
+					const targetDate = new Date();
+					const delta = targetDay - currentDate.getDay();
+					if (delta >= 0) {
+						targetDate.setDate(currentDate.getDate() + delta);
+					} else {
+						targetDate.setDate(currentDate.getDate() + 7 + delta);
+					}
+					selectedDate = moment(targetDate);
+				}
+			}
+		}
+
+		if (!selectedDate) {
+			selectedDate = moment();
+		}
+
+		return selectedDate;
+	}
+}
+
+export default UtilsRest;
