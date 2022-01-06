be0b8f2b7897a29157b9683abe6537b48e8304de
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-06 08:28:00 +0300

    add lessons callback command

---
+++ src/lib/Telegram/commands/callback/mpt/lessons.ts
@@ -0,0 +1,61 @@
+import moment from "moment";
+import { InlineKeyboard } from "puregram";
+
+import utils from "../../../../utils";
+import DB from "../../../../DB";
+
+import telegramUtils from "../../../utils";
+
+import CallbackCommand from "../../../utils/CallbackCommand";
+
+new CallbackCommand({
+	event: "lessons",
+	handler: async (context) => {
+		const selectedDate = moment(context.queryPayload.date, "DD.MM.YYYY");
+
+		if (!selectedDate.isValid()) {
+			return await context.answerCallbackQuery({
+				show_alert: true,
+				text: `Неверная дата ${context.queryPayload.date}`,
+			});
+		}
+
+		if (selectedDate.day() === 0) {
+			return await context.answerCallbackQuery({
+				show_alert: true,
+				text: `${selectedDate.format("DD.MM.YYYY")} воскресенье.`,
+			});
+		}
+
+		const groupName =
+			context.state.user.group ||
+			(context.state.chat ? context.state.chat?.group : "");
+
+		if (groupName === "") {
+			return await context.answerCallbackQuery({
+				show_alert: true,
+				text: "Вы не установили свою группу.",
+			});
+		}
+
+		const group = await DB.api.models.group.findOne({
+			name: groupName,
+		});
+
+		if (!group) {
+			return await context.answerCallbackQuery({
+				show_alert: true,
+				text: "Такой группы не найдено, попробуйте снова установить группу",
+			});
+		}
+
+		const keyboard = telegramUtils.generateKeyboard("lessons");
+		const schedule = await utils.mpt.getGroupSchedule(group, selectedDate);
+
+		await context.message
+			?.editMessageText(`${context.from?.username}, ${schedule.toString()}`, {
+				reply_markup: InlineKeyboard.keyboard(keyboard),
+			})
+			.catch(() => null);
+	},
+});
--- src/lib/Telegram/commands/callbackLoader.ts
+++ src/lib/Telegram/commands/callbackLoader.ts
@@ -1,3 +1,5 @@
 import "./callback/rest/notify";
 import "./callback/rest/profile";
 import "./callback/rest/chat";
+
+import "./callback/mpt/lessons";
