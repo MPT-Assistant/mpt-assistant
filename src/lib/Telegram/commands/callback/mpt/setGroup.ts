cf46672b26c0a25e29328e48e1fc3f51e07a0b49
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-06 08:41:23 +0300

    add telegram setGroup callback command

---
+++ src/lib/Telegram/commands/callback/mpt/setGroup.ts
@@ -0,0 +1,30 @@
+import DB from "../../../../DB";
+
+import CallbackCommand from "../../../utils/CallbackCommand";
+
+new CallbackCommand({
+	event: "setGroup",
+	handler: async (context) => {
+		const selectedGroup = await DB.api.models.group.findOne({
+			name: new RegExp(`^${context.queryPayload.group}$`, "i"),
+		});
+
+		if (!selectedGroup) {
+			return await context.answerCallbackQuery({
+				show_alert: true,
+				text: `Группы ${context.queryPayload.group} не найдено`,
+			});
+		} else {
+			context.state.user.group = selectedGroup.name;
+
+			await context.answerCallbackQuery({
+				show_alert: true,
+				text: `Вы установили себе группу ${selectedGroup.name}.\n(${selectedGroup.specialty})`,
+			});
+
+			return await context.message?.editMessageText(
+				`${context.from?.username}, Вы установили себе группу ${selectedGroup.name}\nОтделение: ${selectedGroup.specialty}`,
+			);
+		}
+	},
+});
