ad67b1ab0ab36006457b79406fafa72d1f29f9d8
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-06 06:01:17 +0300

    add callbackQueryHandler

--- src/lib/Telegram/index.ts
+++ src/lib/Telegram/index.ts
@@ -3,9 +3,11 @@ import { Telegram } from "puregram";
 import DB from "../DB";
 
 import messageHandler from "./handlers/message";
+import callbackQueryHandler from "./handlers/callbackQuery";
 
 const telegram = new Telegram(DB.config.telegram);
 
 telegram.updates.on("message", messageHandler);
+telegram.updates.on("callback_query", callbackQueryHandler);
 
 export default telegram;
