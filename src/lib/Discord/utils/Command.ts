df9ac43da339affe574c6cbab6a234bd96cec84b
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-07 04:32:10 +0300

    add week command

--- src/lib/Discord/utils/Command.ts
+++ src/lib/Discord/utils/Command.ts
@@ -1,10 +1,24 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
 
 import discordUtils from "./index";
 
+type TCommandHandler = (interaction: CommandInteraction) => unknown;
+
+interface ICommandParams {
+	name: string;
+	description: string;
+	handler: TCommandHandler;
+}
+
 class Command extends SlashCommandBuilder {
-	constructor() {
+	public readonly handler: TCommandHandler;
+
+	constructor({ name, description, handler }: ICommandParams) {
 		super();
+		this.handler = handler;
+		this.setName(name);
+		this.setDescription(description);
 		discordUtils.commandsList.push(this);
 	}
 }
