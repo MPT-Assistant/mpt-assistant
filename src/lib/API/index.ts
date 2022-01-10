d124b0138b7d798d54e248a7bbf18db05c361770
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-10 07:33:56 +0300

    create simple fastify server

---
+++ src/lib/API/index.ts
@@ -0,0 +1,27 @@
+import fastify from "fastify";
+import httpsRedirect from "fastify-https-redirect";
+import fastifyFormBody from "fastify-formbody";
+import fastifyMultiPart from "fastify-multipart";
+import fastifyCors from "fastify-cors";
+import fastifyHelmet from "fastify-helmet";
+
+import DB from "../DB";
+
+const server = fastify({
+	https: {
+		key: DB.config.server.key,
+		cert: DB.config.server.cert,
+	},
+});
+
+server.register(httpsRedirect);
+server.register(fastifyFormBody);
+server.register(fastifyMultiPart);
+server.register(fastifyCors, { origin: "*" });
+server.register(fastifyHelmet);
+
+server.get("/ping", (_req, reply) => {
+	reply.send("pong");
+});
+
+export default server;
