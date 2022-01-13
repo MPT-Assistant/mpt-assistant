f45844ed1d636d8f1014e35f9ca77ab56da227a3
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-13 06:08:19 +0300

    fix task (this problem)

--- src/lib/scheduler/tasks/updateReplacementsList.ts
+++ src/lib/scheduler/tasks/updateReplacementsList.ts
@@ -4,6 +4,8 @@ import utils from "../../utils";
 
 export default new Interval({
 	cron: "*/5 * * * *",
-	source: utils.mpt.updateReplacementsList,
+	source: async () => {
+		return await utils.mpt.updateReplacementsList();
+	},
 	type: path.parse(__filename).name,
 });
