b5fcf35fffd1fadc286a1cd955b1803ff07095e9
rus_anonym <alexandrsemin2033@gmail.com>
2022-01-20 09:36:52 +0300

    add schedule.getWeek method

---
+++ src/lib/API/methods/schedule/getWeek.ts
@@ -0,0 +1,46 @@
+import moment from "moment";
+
+import server from "../../index";
+import utils from "../../../utils";
+
+import {
+	Week,
+	ScheduleGetWeekQueryParams,
+	TWeek,
+	TScheduleGetWeekQueryParams,
+} from "../../definitions/schedule";
+
+server.route<{ Querystring: TScheduleGetWeekQueryParams; Reply: TWeek }>({
+	method: ["GET", "POST"],
+	url: "/schedule.getWeek",
+	schema: {
+		querystring: ScheduleGetWeekQueryParams,
+		response: {
+			200: Week,
+		},
+	},
+	handler: async function (request, reply) {
+		const selectedDate = moment(request.query.date);
+		if (!selectedDate.isValid()) {
+			throw new Error("Invalid date");
+		}
+
+		let week: MPT.Week;
+
+		const currentWeekNum = moment().week();
+		if (currentWeekNum % 2 === selectedDate.week() % 2) {
+			week = utils.cache.mpt.week === "Числитель" ? "Числитель" : "Знаменатель";
+		} else {
+			week = utils.cache.mpt.week === "Числитель" ? "Знаменатель" : "Числитель";
+		}
+
+		const response: TWeek = {
+			week,
+			date: selectedDate.valueOf(),
+			isNumerator: week === "Числитель",
+			isDenominator: week === "Знаменатель",
+		};
+
+		return await reply.status(200).send(response);
+	},
+});
