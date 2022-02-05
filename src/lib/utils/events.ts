105fec1c34e6bd9efcf802c7907f0d1116cfbdea
rus_anonym <alexandrsemin2033@gmail.com>
2022-02-05 05:39:32 +0300

    add text_log event

--- src/lib/utils/events.ts
+++ src/lib/utils/events.ts
@@ -1,7 +1,10 @@
+import moment from "moment";
+import { getRandomId } from "vk-io";
 import { EventEmitter } from "events";
 import { ExtractDoc } from "ts-mongoose";
 
 import DB from "../DB";
+import VK from "../VK";
 
 import vkUtils from "../VK/utils/index";
 import telegramUtils from "../Telegram/utils/index";
@@ -14,18 +17,30 @@ interface UtilsEventEmitter {
 			replacement: ExtractDoc<typeof DB.api.schemes.replacementSchema>,
 		) => void,
 	): this;
+	on(event: "text_log", listener: (value: string) => void): this;
 
 	emit(
-		event: string,
+		event: "new_replacement",
 		replacement: ExtractDoc<typeof DB.api.schemes.replacementSchema>,
 	): boolean;
+	emit(event: "text_log", value: string): boolean;
 }
 
 class UtilsEventEmitter extends EventEmitter {
+	private async _textLogHandler(value: string): Promise<void> {
+		const message = `${moment().format("HH:mm:ss, DD.MM.YYYY")} - ${value}`;
+		await VK.api.messages.send({
+			chat_id: DB.config.vk.logs,
+			random_id: getRandomId(),
+			message,
+		});
+	}
+
 	public bindHandlers(): void {
 		this.on("new_replacement", vkUtils.onNewReplacement);
 		this.on("new_replacement", telegramUtils.onNewReplacement);
 		this.on("new_replacement", discordUtils.onNewReplacement);
+		this.on("text_log", this._textLogHandler);
 	}
 }
 
