00ad7684704c4865bda865fbab104748fe804ada
rus_anonym <alexandrsemin2033@gmail.com>
2022-09-04 00:53:33 +0300

    cleanup repository

--- src/lib/Telegram/commands/callbackLoader.ts
+++
@@ -1,8 +0,0 @@
-import "./callback/rest/notify";
-import "./callback/rest/profile";
-import "./callback/rest/chat";
-
-import "./callback/mpt/lessons";
-import "./callback/mpt/replacements";
-import "./callback/mpt/regChat";
-import "./callback/mpt/setGroup";
--- src/lib/scheduler/index.ts
+++
@@ -1,5 +0,0 @@
-import updateReplacementsList from "./tasks/updateReplacementsList";
-import updateCurrentWeek from "./tasks/updateCurrentWeek";
-import updateSchedule from "./tasks/updateSchedule";
-
-export default { updateReplacementsList, updateCurrentWeek, updateSchedule };
