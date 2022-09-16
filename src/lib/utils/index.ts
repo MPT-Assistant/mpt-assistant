5d27dd283386492987765b452e5444dc00cb56fd
rus_anonym <alexandrsemin2033@gmail.com>
2022-09-16 15:24:55 +0300

    add RestUtils

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,14 +1,17 @@
 import { Parser } from "@mpt-assistant/parser";
 
 import MPT from "./lib/MPT";
+import RestUtils from "./lib/rest";
 
 class Utils {
     public parser: InstanceType<typeof Parser>;
-    public mpt: MPT;
+    public mpt: InstanceType<typeof MPT>;
+    public rest: InstanceType<typeof RestUtils>;
 
     constructor() {
         this.parser = new Parser();
         this.mpt = new MPT();
+        this.rest = new RestUtils();
     }
 }
 
