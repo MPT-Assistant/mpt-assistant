3554a1b274523d509c6462195cc132c91a41bfca
rus_anonym <alexandrsemin2033@gmail.com>
2022-09-22 19:01:00 +0300

    add TextCommand

---
+++ src/Bot/Telegram/TextCommand.ts
@@ -0,0 +1,60 @@
+import {
+    Command, ICommandParams, Manager
+} from "@rus-anonym/commands-manager";
+import { MessageContext } from "puregram";
+import utils from "@rus-anonym/utils";
+import { IChat, IUser } from "../../lib/DB/Telegram/types";
+import TelegramBot from ".";
+
+interface ITextCommandState {
+    user: IUser;
+    chat?: IChat;
+}
+
+type TRegExpCommandFunc = (
+    ctx: MessageContext & { state: ITextCommandState },
+    bot: TelegramBot
+) => Promise<unknown>;
+
+class TextCommand extends Command<TRegExpCommandFunc> {
+    private _regex: RegExp;
+
+    constructor(
+        params: ICommandParams<TRegExpCommandFunc> & {
+            trigger: RegExp | string | string[];
+        }
+    ) {
+        super(params);
+
+        if (typeof params.trigger === "string") {
+            params.trigger = new RegExp(
+                `^${utils.regular.escapeRegExp(params.trigger)}`,
+                "i"
+            );
+        }
+
+        if (Array.isArray(params.trigger)) {
+            params.trigger = new RegExp(
+                `^(${params.trigger
+                    .map(utils.regular.escapeRegExp.bind(utils.regular))
+                    .join("|")})`,
+                "i"
+            );
+        }
+
+        this._regex = params.trigger;
+        manager.add(this);
+    }
+
+    public check(value: string): boolean {
+        return this._regex.test(value);
+    }
+}
+
+const manager = new Manager<TextCommand, TRegExpCommandFunc>();
+
+export { manager };
+
+export type { TRegExpCommandFunc, ITextCommandState };
+
+export default TextCommand;
--- src/Bot/Telegram/utils.ts
+++ src/Bot/Telegram/utils.ts
@@ -1,13 +1,17 @@
 import Telegram from ".";
 
+import { manager as textCommandsManager } from "./TextCommand";
 import { manager as callbackCommandsManager } from "./CallbackCommand";
 
 class TelegramUtils {
     private readonly _bot: Telegram;
+    public readonly textCommands: typeof textCommandsManager;
     public readonly callbackCommands: typeof callbackCommandsManager;
 
     constructor(bot: Telegram) {
         this._bot = bot;
+
+        this.textCommands = textCommandsManager;
         this.callbackCommands = callbackCommandsManager;
     }
 }
