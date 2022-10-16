5c086c935b605b5f53b878a0820aaf8067874648
rus_anonym <alexandrsemin2033@gmail.com>
2022-10-17 01:14:40 +0300

    add profile tg callback command

---
+++ src/Bot/Telegram/commands/callback/rest/profile.ts
@@ -0,0 +1,65 @@
+import CallbackCommand from "../../../CallbackCommand";
+import utils from "../../../../../lib/utils";
+import { InlineKeyboard } from "puregram";
+
+new CallbackCommand({
+    trigger: "profile",
+    func: async (ctx): Promise<unknown> => {
+        if (!ctx.state.user.group) {
+            return await ctx.message?.editMessageText(
+                `${ctx.from?.username as string}, Ваш профиль:
+ID: ${ctx.senderId}
+Группа: Не установлена`,
+            );
+        }
+
+        const { specialty } = await utils.mpt.getExtendGroupInfo(
+            ctx.state.user.group,
+        );
+
+        const keyboard = InlineKeyboard.keyboard([
+            !ctx.state.chat
+                ? [
+                    InlineKeyboard.textButton({
+                        text: `${
+                            ctx.state.user.mailings.replacements ? "Отключить" : "Включить"
+                        } уведомления`,
+                        payload: {
+                            cmd: "notify",
+                            status: !ctx.state.user.mailings.replacements,
+                        },
+                    }),
+                ]
+                : [],
+            [
+                InlineKeyboard.urlButton({
+                    text: "Сайт отделения",
+                    url: specialty.url,
+                }),
+            ],
+            [
+                InlineKeyboard.textButton({
+                    text: "Расписание",
+                    payload: { cmd: "lessons", },
+                }),
+            ],
+            [
+                InlineKeyboard.textButton({
+                    text: "Замены",
+                    payload: { cmd: "replacements", },
+                }),
+            ],
+        ]);
+
+        return await ctx.message?.editMessageText(
+            `${ctx.from?.username as string}, Ваш профиль:
+ID: ${ctx.senderId}
+Группа: ${ctx.state.user.group}
+Отделение: ${specialty.name}
+Информирование о заменах: ${
+    ctx.state.user.mailings.replacements ? "Включено" : "Отключено"
+}`,
+            { reply_markup: keyboard, },
+        );
+    },
+});
--- src/Bot/Telegram/commands/callbackLoader.ts
+++ src/Bot/Telegram/commands/callbackLoader.ts
@@ -2,3 +2,5 @@ import "./callback/mpt/lessons";
 import "./callback/mpt/replacements";
 import "./callback/mpt/regChat";
 import "./callback/mpt/setGroup";
+
+import "./callback/rest/profile";
