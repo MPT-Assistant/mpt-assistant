08e8540052c593e63c475e5754eeeb99341cffea
rus_anonym <alexandrsemin2033@gmail.com>
2022-10-20 22:49:35 +0300

    add groups.getList method

--- src/API/loader.ts
+++ src/API/loader.ts
@@ -1,5 +1,6 @@
 import "./methods/schedule/getWeek";
 
 import "./methods/groups/get";
+import "./methods/groups/getList";
 import "./methods/groups/getSpecialty";
 import "./methods/groups/getSpecialtiesList";
