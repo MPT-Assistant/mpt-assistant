b36406ab9d65ae6252baf474637ae98ccc1b4b3c
rus_anonym <alexandrsemin2033@gmail.com>
2022-11-01 19:31:23 +0300

    fix nick command regexp

--- src/Bot/VK/commands/text/rest/nick.ts
+++ src/Bot/VK/commands/text/rest/nick.ts
@@ -1,9 +1,11 @@
 import TextCommand from "../../../TextCommand";
 
+const regex = /^(?:ник)(?:\s(.*)|)$/i;
+
 new TextCommand({
-    trigger: /^(?:ник)(?:\s(.*))?/i,
+    trigger: /^(?:ник)(?:\s(.*)|)$/i,
     func: async (ctx, bot): Promise<unknown> => {
-        const args = ctx.text?.match(/^(?:ник)(?:\s(.*))$/i);
+        const args = ctx.text?.match(regex);
         if (!args) {
             const [userInfo] = (await bot.instance.api.users.get({ user_ids: [ctx.senderId.toString()] })) as [{first_name: string}];
             ctx.state.user.nickname = userInfo.first_name;
