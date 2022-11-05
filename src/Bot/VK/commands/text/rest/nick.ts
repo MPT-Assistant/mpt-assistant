08ea263957ccf5a2e9a8b895045a040af65fa990
rus_anonym <alexandrsemin2033@gmail.com>
2022-11-05 04:08:31 +0300

    close #53

--- src/Bot/VK/commands/text/rest/nick.ts
+++ src/Bot/VK/commands/text/rest/nick.ts
@@ -3,10 +3,10 @@ import TextCommand from "../../../TextCommand";
 const regex = /^(?:ник)(?:\s(.*)|)$/i;
 
 new TextCommand({
-    trigger: /^(?:ник)(?:\s(.*)|)$/i,
+    trigger: regex,
     func: async (ctx, bot): Promise<unknown> => {
         const args = ctx.text?.match(regex);
-        if (!args) {
+        if (!args || !args[1]) {
             const [userInfo] = (await bot.instance.api.users.get({ user_ids: [ctx.senderId.toString()] })) as [{first_name: string}];
             ctx.state.user.nickname = userInfo.first_name;
             return ctx.reply("ник сброшен");
